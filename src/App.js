79b16cb6f6cd518b5787021fb8975c0f0014092e
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-11-06 18:49:39 -0600

    thank you SuboptimalEng

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import * as THREE from 'three';
+import React, { useState, useEffect } from 'react';
+import { findAllInRenderedTree } from 'react-dom/test-utils';
+import SceneInit from './lib/SceneInit';
 function App() {
+  useEffect(() => {
+    const test = new SceneInit('world');
+    test.initialize();
+    test.animate();
+
+    const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+    const boxMaterial = new THREE.MeshNormalMaterial();
+    const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+    test.scene.add(boxMesh);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas id="world"></canvas>
     </div>
   );
 }
