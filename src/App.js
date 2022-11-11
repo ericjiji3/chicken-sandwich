0fcc0f4001f5168c380f710f980daabdf581fc40
ericjiji3 <69319444+ericjiji3@users.noreply.github.com>
2022-11-10 21:04:42 -0600

    hie

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import * as THREE from 'three';
 import React, {useEffect } from 'react';
 import SceneInit from './lib/SceneInit'; 
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import chicken from './images/chicken.gltf';
 
 function App() {
   useEffect(() => {
@@ -16,21 +17,20 @@ function App() {
     boxMesh.position.z = 8;
     boxMesh.receiveShadow = true;
 
-    const loader = new GLTFLoader();
-    loader.load( '/src/images/chicken.gltf' , function ( gltf ) {
-      world.scene.add( gltf.scene );
-    }, undefined, function ( error ) {
-      console.error( error );
-    } );
+    // const loader = new GLTFLoader();
+    // loader.load( chicken , function ( gltf ) {
+    //   world.scene.add( gltf.scene );
+    // }, undefined, function ( error ) {
+    //   console.error( error );
+    // } );
 
-    const floor = new THREE.PlaneGeometry(1000, 1000);
-    let heightTexture = new THREE.TextureLoader().load('./images/height-map.png');
+    const floor = new THREE.PlaneGeometry(1000, 1000, 500, 500);
+    let heightTexture = new THREE.TextureLoader().setPath('./images/').load('height-map.png');
     heightTexture.wrapS = heightTexture.wrapT = THREE.RepeatWrapping;
     const floorMaterial = new THREE.MeshStandardMaterial({
       color: 0xffffff,
-      // wireframe: true,
-      // displacementMap: heightTexture,
-      // displacementScale: 1
+      displacementMap: heightTexture,
+      displacementScale: 1
     })
     const floorMesh = new THREE.Mesh(floor, floorMaterial);
 
